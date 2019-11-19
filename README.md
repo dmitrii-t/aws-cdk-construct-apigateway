af53b93714a6fd9a85b68419320d86945c83184d
Dmitrii Tcimbal <dtcimbal@gmail.com>
2019-11-19 20:25:55 +1100

    Initial

---
+++ README.md
@@ -0,0 +1,5 @@
+### Neat 'n' sweet :lollipop: AWS ApiGateway construct to build cloud apps
+
+```bash
+npm run tst
+```
---
+++ src/index.spec.ts
@@ -0,0 +1,70 @@
+// Integration test
+import {ApiGatewayBuilder} from './index';
+import {App, CfnOutput, Stack} from '@aws-cdk/core';
+import {deployStack, destroyStack, withStack} from 'cdk-util';
+import fetch from 'node-fetch';
+import {expect} from 'chai';
+import * as path from 'path';
+
+/**
+ * CDK output directory
+ */
+const CdkOut = path.resolve('cdk.out');
+
+describe('apigateway-test', () => {
+  /**
+   * Stack to deploy the construct for tests
+   */
+  class ApiGatewayTest extends Stack {
+    constructor(scope: App, id: string = ApiGatewayTest.name) {
+      super(scope, id);
+
+      const producingConstruct = new ApiGatewayBuilder(this, {id: 'ApiGatewayTest'});
+
+      // Child resource one
+      producingConstruct.resource('/test/child/one')
+        .addCors({allowOrigins: ['*'], allowMethods: ['GET']})
+        .status200('GET');
+
+      // Child resource two
+      producingConstruct.resource('/test/child/two')
+        .addCors({allowOrigins: ['*'], allowMethods: ['GET']})
+        .status200('GET');
+
+      // Outputs
+      new CfnOutput(this, 'ResourceOneUrl', {value: producingConstruct.resourceUrl('/test/child/one')});
+      new CfnOutput(this, 'ResourceOneHandlerName', {value: producingConstruct.resource('/test/child/one').handlerName!!});
+
+      new CfnOutput(this, 'ResourceTwoUrl', {value: producingConstruct.resourceUrl('/test/child/two')});
+      new CfnOutput(this, 'ResourceTwoHandlerName', {value: producingConstruct.resource('/test/child/two').handlerName!!});
+    }
+  }
+
+  const id = 'ApiGatewayTest';
+  const app = new App({outdir: CdkOut});
+  const stack = new ApiGatewayTest(app, id);
+
+  // Setup task
+  before(async () => {
+    await deployStack({name: id, app, exclusively: true});
+  });
+
+  // Cleanup task
+  after(async () => {
+    await destroyStack({name: id, app, exclusively: true});
+  });
+
+  it('should get response from apigateway for both resources', withStack({name: id, app, exclusively: true}, async ({environment, stack}) => {
+    // Given
+    const resourceOneUrl = stack.Outputs!!.find(it => it.OutputKey === 'ResourceOneUrl')!!.OutputValue;
+    const resourceTwoUrl = stack.Outputs!!.find(it => it.OutputKey === 'ResourceTwoUrl')!!.OutputValue;
+
+    // Then
+    const responseOne = await fetch(resourceOneUrl!!);
+    expect(responseOne.status).to.equal(200);
+
+    const responseTwo = await fetch(resourceTwoUrl!!);
+    expect(responseTwo.status).to.equal(200);
+  }));
+});
+
