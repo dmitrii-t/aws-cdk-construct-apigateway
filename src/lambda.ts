32b71729a44e7fadf4c92ca773790ba90bb3a681
Dmitrii Tcimbal <dtcimbal@gmail.com>
2019-12-10 18:15:50 +1100

    Adds a builder to build ApiGateway event handler

--- src/lambda.ts
+++ src/lambda.ts
@@ -1,23 +1,24 @@
 import {APIGatewayEvent, APIGatewayProxyResult} from 'aws-lambda';
 import {Body} from "./Body";
 
-export async function handleApiGatewayEvent(event: APIGatewayEvent): Promise<APIGatewayProxyResult> {
-  console.log(`Handled ApiGateway Event ${JSON.stringify(event)}`);
+export function withApiGatewayEventHandler(bodyHandler: (body: Body) => void) {
+  return async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
+    console.log(`Handled ApiGateway Event ${JSON.stringify(event)}`);
 
-  try {
-    const body = JSON.parse(event.body);
-    return await handleBody(body);
+    try {
+      const body = JSON.parse(event.body);
+      await bodyHandler(body);
 
-  } catch (e) {
-    console.error(`Fail to process a body of ApiGateway event ${event.body}`, e);
+      return {
+        statusCode: 200
+      } as APIGatewayProxyResult
+
+    } catch (e) {
+      console.error(`Fail to process a body of ApiGateway event ${event.body}`, e);
+    }
   }
 }
 
-async function handleBody(body: Body): Promise<APIGatewayProxyResult> {
+export const handleApiGatewayEvent = withApiGatewayEventHandler((body) => {
   console.log(`processing message body ${JSON.stringify(body)}`);
-  //TODO extension point
-
-  return {
-    statusCode: 200
-  } as APIGatewayProxyResult
-}
+});
